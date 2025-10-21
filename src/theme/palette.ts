e640f012f6ef469af3cea6786a13923e84388fa7
Rakib Ahmed <sujonahmed45a4@gmail.com>
2025-10-21 18:26:10 +0600

    project initiate done

---
+++ src/theme/palette.ts
@@ -0,0 +1,15 @@
+import { PaletteOptions } from "@mui/material/styles";
+
+export const palette: PaletteOptions = {
+  primary: { main: "#000" },
+  success: { main: "#12B76A" },
+  background: {
+    default: "#fafbff",
+    paper: "#F2F2F2",
+  },
+  text: {
+    primary: "#111",
+    secondary: "#555",
+  },
+  divider: "#ebebeb",
+};
